f8792d7c9f395218c5b6e6c8700ca9d29a52653c
Simon McCartney <smccartney@axon.com>
2019-01-31 23:27:42 +0000

    wip

--- base/tasks/baseline.sh
+++ base/tasks/baseline.sh
@@ -9,3 +9,6 @@ echo "---- Update and Upgrade"
 sudo yum upgrade -y
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 sudo yum install -y curl unzip zip jq
+
+echo "---- Install SSH keys"
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAmdv+rdGgJ7FmEZkVUH7eBpg0IAkXNLMg2srv38BQypm0kdhIBBm6Tp+1EkDNJ8cLmxJQCPQmA2R1ObD0LYsDBOVzvRYx1I49kNUJMXpO0vltf/66zQSWBNHH4DuGqRGqUwxVqL63TL48rfIP7I5lw3o1KSoqtxqvhEiYzvitzVc= simonm@vanquish" | sudo tee /home/centos/.ssh/authorized_keys
